4d5536c6c8473e83acd7e3b6ba1df1853cdcc485
cyprientemateu <cyprien.temateu@yahoo.com>
2025-05-18 01:26:06 -0500

    Initial CI/CD setup

--- demo-project5/terraform/main.tf
+++ demo-project5/terraform/main.tf
@@ -1,20 +1,16 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
+variable "public_key" {}
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "ci-cd-key"
+  key_name   = "tcc-ec2-key"
   public_key = var.public_key
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (update if needed)
+  ami           = "ami-0c02fb55956c7d316" # example for Amazon Linux 2 in us-east-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
 
   tags = {
-    Name = "CI-CD-Web"
+    Name = "TCC-Web"
   }
 }
