00df329f953ca0f498a1942501990ac11160e1e0
cyprientemateu <cyprien.temateu@yahoo.com>
2025-05-18 13:54:19 -0500

    Initial CI/CD setup

--- demo-project5/terraform/main.tf
+++ demo-project5/terraform/main.tf
@@ -1,14 +1,54 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 resource "aws_key_pair" "deployer" {
   key_name   = "tcc-ec2-key-test2"
   public_key = var.public_key
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # example for Amazon Linux 2 in us-east-1
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  ami                    = "ami-0c02fb55956c7d316" # example for Amazon Linux 2 in us-east-1
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "TCC-Web"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "tcc-web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
